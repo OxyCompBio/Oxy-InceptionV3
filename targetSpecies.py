7e76558f625a35c4dcda6a3bf651e1f19b0a7093
Maya-Gonzalez <mayagonz56@gmail.com>
2022-03-24 14:30:59 -0700

    Added targetSpecies.py

--- targetSpecies.py
+++ targetSpecies.py
@@ -3,6 +3,8 @@
 #   So now we have just 1 megadetector detection
 #       Cropped images (using MD boxes)
 #           80/20 train/test split
+from audioop import ulaw2lin
+import os
 import csv
 import json
 import numpy as np
@@ -25,43 +27,55 @@ MCTargetSpeciesDetections.to_csv('MCTargetSpecies.csv')
 
 allBboxStr = MCTargetSpeciesDetections.loc[:]['jsonAnimalDetection']
 width = 3840 
-height = 2160
+height = 2880
+# height = 2160
+# width, height = im.size
+# print(width, height)
 def convertToPixels (ann):
     ann = eval(ann)
+    width = 3840 
+    height = 2880
+
     ann['bbox'][0] = ann['bbox'][0] * width
     ann['bbox'][1] = ann['bbox'][1] * height
     ann['bbox'][2] = ann['bbox'][2] * width
     ann['bbox'][3] = ann['bbox'][3] * height
     return ann
 MCTargetSpeciesDetections.loc[:, 'jsonAnimalDetection']  = MCTargetSpeciesDetections['jsonAnimalDetection'].apply(convertToPixels)
-# MCTargetSpeciesDetections.to_csv('finalOutput.csv')
-print(MCTargetSpeciesDetections.loc[:, 'jsonAnimalDetection'])
 
 
-# loop over each row, call crop method and save output(cropped image) to cropped folder. save cropped file name as new column (croppedFilename - val is img/croppedFiles)
-# use coordinates for left, right, top, bottom
-
-# df = pd.read_csv('finalOutput.csv')
-# create new folder called croppedFilename
-
-# add new column to csv called croppedImage
-# for each row, crop image
 filename = []
-for row in MCTargetSpeciesDetections:
-    imgName = MCTargetSpeciesDetections.loc[row, 'photoName']
-    coords = MCTargetSpeciesDetections.loc[row, 'jsonAnimalDetection']
-    # open image from photoUpload
-    im = Image.open(imgName)
-    im = im.crop(coords[0], coords[1], coords[2], coords[3])
-    image_path = '/home/compbio/GDrive/croppedOutput/' + imgName + '.jpg'
-    im.save(image_path, 'JPEG')
+MCTargetSpeciesDetections.reset_index()
+imgName = MCTargetSpeciesDetections.loc[:]['photoName']
+coords = MCTargetSpeciesDetections.loc[:]['jsonAnimalDetection']
+
+
+im = Image.open('/home/compbio/GDrive/UWIN_Test_Dataset/Photo_Upload/first_upload/VID626-00000.jpg')
+countImg=0
+for image in imgName:
+    if os.path.exists('/home/compbio/GDrive/UWIN_Test_Dataset/Photo_Upload/first_upload/'+ image):
+        im = Image.open('/home/compbio/GDrive/UWIN_Test_Dataset/Photo_Upload/first_upload/' + image)
+    if os.path.exists('/home/compbio/GDrive/UWIN_Test_Dataset/Photo_Upload/second_upload'+ image):
+        im = Image.open('/home/compbio/GDrive/UWIN_Test_Dataset/Photo_Upload/second_upload/' + image)
+    if os.path.exists('/home/compbio/GDrive/UWIN_Test_Dataset/Photo_Upload/third_upload'+ image):
+        im = Image.open('/home/compbio/GDrive/UWIN_Test_Dataset/Photo_Upload/third_upload/' + image)
+    if os.path.exists('/home/compbio/GDrive/UWIN_Test_Dataset/Photo_Upload/fourth_upload/' + image):
+        im = Image.open('/home/compbio/GDrive/UWIN_Test_Dataset/Photo_Upload/fourth_upload/' + image)
+    
+    truewidth, trueheight = im.size
+    if (truewidth != width  and trueheight != height):
+        continue
+
+    ulx = coords.iloc[countImg]['bbox'][0]
+    uly = coords.iloc[countImg]['bbox'][1]
+    pwidth = coords.iloc[countImg]['bbox'][2]
+    pheight = coords.iloc[countImg]['bbox'][3]
+
+    crop_img = im.crop((ulx, uly, ulx+pwidth, uly+pheight))
+    image_path = '/home/compbio/GDrive/croppedOutput/' + image
+    crop_img.save(image_path, 'JPEG')
+    print('saved image path = ', image_path, '\ncoords', ulx, uly, pwidth, pheight)
 
     filename.append(image_path)
-# create filename
-# pass filename value into new column
+    countImg +=1
 
-
-
-MCTargetSpeciesDetections['croppedFilename'] = filename
-
-MCTargetSpeciesDetections
