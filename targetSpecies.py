a03324aa0a206c25a9974897f1c40197086e81f8
Maya-Gonzalez <mayagonz56@gmail.com>
2022-04-14 15:07:10 -0700

    Add files via upload

--- targetSpecies.py
+++ targetSpecies.py
@@ -1,8 +1,6 @@
-# Take megaspecies.csv to get list of target species, get list of target species to become labels (STEP 1- list of target species names)
-# Filter out non-target species, then filter to mD crop photos
-#   So now we have just 1 megadetector detection
-#       Cropped images (using MD boxes)
-#           80/20 train/test split
+# Take megaspecies.csv to get list of target species,
+# take input data (csv) and filter out non-target species, then filter to mD crop photos
+
 from audioop import ulaw2lin
 import os
 import csv
@@ -10,72 +8,107 @@ import json
 import numpy as np
 import pandas as pd
 from PIL import Image
-# create virtual Env to install np
-
+# create list of target species 
 df = pd.read_csv('/home/compbio/GDrive/UWIN_Test_Dataset/MegaSpecies.csv', dtype = str) 
 df = df[df.target == 'Target']
 targetSpecies = df.loc[:, ['commonName','target']]
 # use MC BQ to filter out rows with non-target species and numDetections != 1
-df = pd.read_csv('/home/compbio/GDrive/UWIN_Test_Dataset/multicity-bigquery-export.csv', dtype = str) 
-df['numAnimalDetections'] = pd.to_numeric(df['numAnimalDetections'])
-df= df[df.commonName.isin(targetSpecies.loc[:]['commonName'])]
-MCTargetSpeciesDetections = df[df['numAnimalDetections'] == 1]
-# print(MCTargetSpeciesDetections.loc[:]['jsonAnimalDetection'])
-#print(MCTargetSpeciesDetections.loc[: , ['commonName', 'numAnimalDetections']])
-MCTargetSpeciesDetections.to_csv('MCTargetSpecies.csv')
 
+# df = pd.read_csv('/home/compbio/GDrive/UWIN_Test_Dataset/multicity-bigquery-export.csv', dtype = str) 
+df = pd.read_csv('/home/compbio/mayaDir/ast2.csv', dtype = str) 
+df['numAnimalDetections'] = pd.to_numeric(df['numAnimalDetections'])
+#UNCOMMENT BELOW when you switch files, we only want target species. only include if commonName is in mataData
+# df= df[df.commonName.isin(targetSpecies.loc[:]['commonName'])]
+MCTargetSpeciesDetections = df[df['numAnimalDetections'] == 1]
 
 allBboxStr = MCTargetSpeciesDetections.loc[:]['jsonAnimalDetection']
-width = 3840 
-height = 2880
+width = df['imgWidth']
+height = df['imgHeight']
+# width = 3840 
 # height = 2160
-# width, height = im.size
-# print(width, height)
+
 def convertToPixels (ann):
     ann = eval(ann)
     width = 3840 
-    height = 2880
+    height = 2160
 
-    ann['bbox'][0] = ann['bbox'][0] * width
-    ann['bbox'][1] = ann['bbox'][1] * height
-    ann['bbox'][2] = ann['bbox'][2] * width
-    ann['bbox'][3] = ann['bbox'][3] * height
+    ulx = ann['bbox'][0] * width 
+    uly = ann['bbox'][1] * height 
+    brx = ulx + ann['bbox'][2]*width 
+    bry = uly + ann['bbox'][3]*height 
+
+    ann['bbox'][0] = ann['bbox'][0] * width 
+    ann['bbox'][1] = ann['bbox'][1] * height 
+    ann['bbox'][2] = ulx + ann['bbox'][2]*width 
+    ann['bbox'][3] = uly + ann['bbox'][3]*height 
     return ann
 MCTargetSpeciesDetections.loc[:, 'jsonAnimalDetection']  = MCTargetSpeciesDetections['jsonAnimalDetection'].apply(convertToPixels)
 
 
 filename = []
 MCTargetSpeciesDetections.reset_index()
-imgName = MCTargetSpeciesDetections.loc[:]['photoName']
+imgName = MCTargetSpeciesDetections.loc[:]['photoDir']
 coords = MCTargetSpeciesDetections.loc[:]['jsonAnimalDetection']
 
-
-im = Image.open('/home/compbio/GDrive/UWIN_Test_Dataset/Photo_Upload/first_upload/VID626-00000.jpg')
 countImg=0
 for image in imgName:
-    if os.path.exists('/home/compbio/GDrive/UWIN_Test_Dataset/Photo_Upload/first_upload/'+ image):
-        im = Image.open('/home/compbio/GDrive/UWIN_Test_Dataset/Photo_Upload/first_upload/' + image)
-    if os.path.exists('/home/compbio/GDrive/UWIN_Test_Dataset/Photo_Upload/second_upload'+ image):
-        im = Image.open('/home/compbio/GDrive/UWIN_Test_Dataset/Photo_Upload/second_upload/' + image)
-    if os.path.exists('/home/compbio/GDrive/UWIN_Test_Dataset/Photo_Upload/third_upload'+ image):
-        im = Image.open('/home/compbio/GDrive/UWIN_Test_Dataset/Photo_Upload/third_upload/' + image)
-    if os.path.exists('/home/compbio/GDrive/UWIN_Test_Dataset/Photo_Upload/fourth_upload/' + image):
-        im = Image.open('/home/compbio/GDrive/UWIN_Test_Dataset/Photo_Upload/fourth_upload/' + image)
-    
+    im = Image.open(image)
+
     truewidth, trueheight = im.size
-    if (truewidth != width  and trueheight != height):
-        continue
+    # if (truewidth != width  and trueheight != height):
+    #     continue
 
-    ulx = coords.iloc[countImg]['bbox'][0]
-    uly = coords.iloc[countImg]['bbox'][1]
-    pwidth = coords.iloc[countImg]['bbox'][2]
-    pheight = coords.iloc[countImg]['bbox'][3]
-
-    crop_img = im.crop((ulx, uly, ulx+pwidth, uly+pheight))
-    image_path = '/home/compbio/GDrive/croppedOutput/' + image
+    bbox = coords.iloc[countImg]['bbox']
+    crop_img = im.crop((bbox[0], bbox[1], bbox[2], bbox[3]))
+    
+    # save cropped image to new dir 
+    image_name = os.path.basename(image)
+    image_path = '/home/compbio/GDrive/croppedOutput/' + image_name
     crop_img.save(image_path, 'JPEG')
-    print('saved image path = ', image_path, '\ncoords', ulx, uly, pwidth, pheight)
-
+    # FIXME add filename list as column in csv
     filename.append(image_path)
     countImg +=1
 
+
+
+    # get path of photoUpload, uncomment of using UWIN dataset. Not necessary is photos are all in one dir/folder
+    # if os.path.exists('/home/compbio/GDrive/UWIN_Test_Dataset/Photo_Upload/first_upload/'+ image):
+    #     im = Image.open('/home/compbio/GDrive/UWIN_Test_Dataset/Photo_Upload/first_upload/' + image)
+    #     print('first_upload')
+    # if os.path.exists('/home/compbio/GDrive/UWIN_Test_Dataset/Photo_Upload/second_upload'+ image):
+    #     im = Image.open('/home/compbio/GDrive/UWIN_Test_Dataset/Photo_Upload/second_upload/' + image)
+    #     print('second_upload')
+    # if os.path.exists('/home/compbio/GDrive/UWIN_Test_Dataset/Photo_Upload/third_upload'+ image):
+    #     im = Image.open('/home/compbio/GDrive/UWIN_Test_Dataset/Photo_Upload/third_upload/' + image)
+    #     print('third_upload')
+    # if os.path.exists('/home/compbio/GDrive/UWIN_Test_Dataset/Photo_Upload/fourth_upload/' + image):
+    #     im = Image.open('/home/compbio/GDrive/UWIN_Test_Dataset/Photo_Upload/fourth_upload/' + image)
+    #     print('fourth_upload')
+    
+
+
+
+
+
+    # ymin = ann['bbox'][1]
+    # ymax = ymin + ann['bbox'][3]
+    # xmin = ann['bbox'][0]
+    # xmax = xmin + ann['bbox'][2]
+
+#     ymax = ann['bbox'][1]
+#     ymin = ymax + ann['bbox'][3]
+#     xmax = ann['bbox'][0]
+#     xmin = xmax + ann['bbox'][2]
+#     print(ann)
+#     print(ymin, ymax, xmin, xmax)
+# # 608, 1213, 897, 1833
+#     ann['bbox'][3] = ymin * height 
+#     ann['bbox'][1] = ymax * height 
+#     ann['bbox'][0] = xmin * width 
+#     ann['bbox'][2] = xmax * width
+
+    # ulx = coords.iloc[countImg]['bbox'][0]
+    # uly = coords.iloc[countImg]['bbox'][1]
+    # pwidth = coords.iloc[countImg]['bbox'][2]
+    # pheight = coords.iloc[countImg]['bbox'][3]
+    # crop_img = im.crop((ulx, uly, ulx+pwidth, uly+pheight))
